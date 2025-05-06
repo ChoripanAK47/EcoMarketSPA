ea7523859fae6c653a75f6c57b6717f3e1779c64
ChoripanAK47 <tomasolmosvasquez@gmail.com>
2025-05-06 12:19:12 -0400

    Conexion a base de datos (falta terminar)

--- src/main/java/com/ecomarket/spa/EcoMarketSPA/Usuario/Model/Usuario.java
+++ src/main/java/com/ecomarket/spa/EcoMarketSPA/Usuario/Model/Usuario.java
@@ -1,23 +1,47 @@
 package com.ecomarket.spa.EcoMarketSPA.Usuario.Model;
 
 import lombok.Data;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 
+@Entity
+@Table(name = "Usuario")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 
 public class Usuario {
+
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    @Column(nullable = false)
     private String nombre;
+    @Column(nullable = false)
     private String apellido;
+    @Column(unique = true, length = 13, nullable = false)
+    private String rut;
+    @Column(nullable = false)
     private String email;
+    @Column(nullable = false)
     private String password;
+    @Column(nullable = false)
     private String telefono;
+    @Column(nullable = false)
     private String direccion;
+    @Column(nullable = false)
     private String region;
+    @Column(nullable = false)
     private String ciudad;
+    @Column(nullable = false)
     private String codigoPostal;
+    @Column(nullable = false)
     private boolean admin; // true si es administrador, false si es cliente
 }
